665479f30d131bd44a823a87a34c09dcc14a4c11
RajaKing2894 <rajasri2609@gmail.com>
2023-02-08 22:32:26 +0530

    Sam Code Added

--- src/org/pac/Practice.java
+++ src/org/pac/Practice.java
@@ -1,6 +1,9 @@
 package org.pac;
 
 public class Practice {
+	private void SamMethod() {
+		System.out.println("Sam...");
+	}
 	public static void main(String[] args) {
 		int a=100,b=200;
 		System.out.println(a+b);
